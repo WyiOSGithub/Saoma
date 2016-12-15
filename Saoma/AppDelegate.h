a3bdcbc7b749ad24f859e05aae3e382c9a084455
wangyu <553574756@qq.com>
2016-12-15 11:47:31 +0800

    all

---
+++ Saoma/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Saoma
+//
+//  Created by wang yu on 16/9/1.
+//  Copyright © 2016年 wang yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Saoma/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Saoma
+//
+//  Created by wang yu on 16/9/1.
+//  Copyright © 2016年 wang yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
